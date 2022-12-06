e35db8ab436deb5bcf861b717316a23f5aa2b4a2
mikiasWhlg <mikiaszenebe8@gmial.com>
2022-12-06 22:06:43 +0300

    modified

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -3,7 +3,6 @@ import React from "react";
 import html from "../assets/html.png";
 import css from "../assets/css.png";
 import javascript from "../assets/javascript.png";
-import reactImage from "../assets/react.png";
 
 import github from "../assets/github.png";
 import tailwind from "../assets/tailwind.png";
